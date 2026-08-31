9f65be14896f3237b53c7445e9b5aba95bbade14
M-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-08-31 13:57:00 +0600

    Update ProductEntryForm.Designer.cs

--- ShopSystem/ProductEntryForm.Designer.cs
+++ ShopSystem/ProductEntryForm.Designer.cs
@@ -29,28 +29,164 @@
         private void InitializeComponent()
         {
             this.btnBackfromPE = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.cmbCetagory = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBackfromPE
             // 
             this.btnBackfromPE.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnBackfromPE.Location = new System.Drawing.Point(633, 384);
+            this.btnBackfromPE.Location = new System.Drawing.Point(844, 473);
+            this.btnBackfromPE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBackfromPE.Name = "btnBackfromPE";
-            this.btnBackfromPE.Size = new System.Drawing.Size(155, 54);
+            this.btnBackfromPE.Size = new System.Drawing.Size(207, 66);
             this.btnBackfromPE.TabIndex = 0;
             this.btnBackfromPE.Text = "Back";
             this.btnBackfromPE.UseVisualStyleBackColor = false;
             this.btnBackfromPE.Click += new System.EventHandler(this.btnBackfromPE_Click);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.btnClean);
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.cmbCetagory);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtPrice);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtCode);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(525, 554);
+            this.panel2.TabIndex = 10;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(227, 384);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(99, 38);
+            this.btnClean.TabIndex = 12;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(332, 384);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 38);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.linkLabel1.Location = new System.Drawing.Point(167, 36);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(159, 29);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Add Product ";
+            // 
+            // cmbCetagory
+            // 
+            this.cmbCetagory.FormattingEnabled = true;
+            this.cmbCetagory.Location = new System.Drawing.Point(212, 263);
+            this.cmbCetagory.Name = "cmbCetagory";
+            this.cmbCetagory.Size = new System.Drawing.Size(182, 24);
+            this.cmbCetagory.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(107, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cetagory";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(214, 207);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(180, 22);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(107, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Price";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(214, 150);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(107, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(214, 100);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(180, 22);
+            this.txtCode.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(107, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code";
+            // 
             // ProductEntryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.btnBackfromPE);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ProductEntryForm";
             this.Text = "Product Entry";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ProductEntryForm_FormClosed);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +194,17 @@
         #endregion
 
         private System.Windows.Forms.Button btnBackfromPE;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox cmbCetagory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label1;
     }
 }
