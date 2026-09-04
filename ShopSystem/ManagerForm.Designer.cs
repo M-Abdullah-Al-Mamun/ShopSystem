0ce80d1c4771329b266401f53ce8be367d007466
Azmain Rafi <rafi.azmain9@gmail.com>
2026-09-04 22:18:29 +0600

    ManagerForm was Added

---
+++ ShopSystem/ManagerForm.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace ShopSystem
+{
+    partial class ManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagerForm));
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShowData = new System.Windows.Forms.Button();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Productname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prdouctcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.gridShowData = new System.Windows.Forms.DataGridView();
+            this.txtExit = new System.Windows.Forms.Button();
+            this.btnout = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblManagerUsername = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridShowData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 365);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 45);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Sale Entry";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(19, 218);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(118, 45);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add New product";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnShowData
+            // 
+            this.btnShowData.Location = new System.Drawing.Point(19, 147);
+            this.btnShowData.Name = "btnShowData";
+            this.btnShowData.Size = new System.Drawing.Size(118, 45);
+            this.btnShowData.TabIndex = 11;
+            this.btnShowData.Text = "Show Product List";
+            this.btnShowData.UseVisualStyleBackColor = true;
+            // 
+            // Quantity
+            // 
+            this.Quantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.MinimumWidth = 6;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "Price";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Productname
+            // 
+            this.Productname.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Productname.DataPropertyName = "ProductName";
+            this.Productname.HeaderText = "Prdouct Name";
+            this.Productname.MinimumWidth = 6;
+            this.Productname.Name = "Productname";
+            this.Productname.ReadOnly = true;
+            // 
+            // Prdouctcode
+            // 
+            this.Prdouctcode.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Prdouctcode.DataPropertyName = "ProductCode";
+            this.Prdouctcode.HeaderText = "Prdouct Code";
+            this.Prdouctcode.MinimumWidth = 6;
+            this.Prdouctcode.Name = "Prdouctcode";
+            this.Prdouctcode.ReadOnly = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(19, 294);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(118, 45);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete Product";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(330, 455);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 27);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Visible = false;
+            // 
+            // gridShowData
+            // 
+            this.gridShowData.AllowUserToAddRows = false;
+            this.gridShowData.AllowUserToDeleteRows = false;
+            this.gridShowData.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gridShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridShowData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Prdouctcode,
+            this.Productname,
+            this.Price,
+            this.Quantity});
+            this.gridShowData.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gridShowData.Location = new System.Drawing.Point(0, -1);
+            this.gridShowData.Name = "gridShowData";
+            this.gridShowData.ReadOnly = true;
+            this.gridShowData.RowHeadersWidth = 51;
+            this.gridShowData.RowTemplate.Height = 24;
+            this.gridShowData.Size = new System.Drawing.Size(954, 453);
+            this.gridShowData.TabIndex = 0;
+            // 
+            // txtExit
+            // 
+            this.txtExit.BackColor = System.Drawing.Color.Red;
+            this.txtExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExit.Location = new System.Drawing.Point(1019, 5);
+            this.txtExit.Name = "txtExit";
+            this.txtExit.Size = new System.Drawing.Size(90, 32);
+            this.txtExit.TabIndex = 8;
+            this.txtExit.Text = "Exit";
+            this.txtExit.UseVisualStyleBackColor = false;
+            // 
+            // btnout
+            // 
+            this.btnout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnout.Location = new System.Drawing.Point(923, 5);
+            this.btnout.Name = "btnout";
+            this.btnout.Size = new System.Drawing.Size(90, 32);
+            this.btnout.TabIndex = 7;
+            this.btnout.Text = "Logout";
+            this.btnout.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblManagerUsername
+            // 
+            this.lblManagerUsername.AutoSize = true;
+            this.lblManagerUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManagerUsername.ForeColor = System.Drawing.Color.Transparent;
+            this.lblManagerUsername.Location = new System.Drawing.Point(52, 15);
+            this.lblManagerUsername.Name = "lblManagerUsername";
+            this.lblManagerUsername.Size = new System.Drawing.Size(109, 22);
+            this.lblManagerUsername.TabIndex = 3;
+            this.lblManagerUsername.Text = "User Name";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Blue;
+            this.panel2.Controls.Add(this.txtExit);
+            this.panel2.Controls.Add(this.btnout);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.lblManagerUsername);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 598);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(167, 44);
+            this.panel2.TabIndex = 9;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(171, 458);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(150, 24);
+            this.txtSearch.TabIndex = 13;
+            this.txtSearch.Visible = false;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.ForeColor = System.Drawing.Color.Transparent;
+            this.lblSearch.Location = new System.Drawing.Point(2, 457);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(148, 22);
+            this.lblSearch.TabIndex = 12;
+            this.lblSearch.Text = "Search Product";
+            this.lblSearch.Visible = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(991, 68);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(118, 34);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 41);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Manager Window";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.gridShowData);
+            this.panel3.Controls.Add(this.txtSearch);
+            this.panel3.Controls.Add(this.lblSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(167, 109);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(954, 533);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.btnRefresh);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1121, 109);
+            this.panel1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 45);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Sale Summary";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 45);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Sale Details";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 642);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnShowData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "ManagerForm";
+            this.Text = "ManagerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.gridShowData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShowData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Productname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prdouctcode;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView gridShowData;
+        private System.Windows.Forms.Button txtExit;
+        private System.Windows.Forms.Button btnout;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblManagerUsername;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
--- ShopSystem/Program.cs
+++ ShopSystem/Program.cs
@@ -16,7 +16,7 @@ namespace ShopSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new ManagerForm());
         }
     }
 }
