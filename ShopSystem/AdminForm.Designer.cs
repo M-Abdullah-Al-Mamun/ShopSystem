66c74c57739179237e0df3f3c6bf0b8d27d6f82a
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-06 22:51:15 +0600

    Update AdminForm.Designer.cs

--- ShopSystem/AdminForm.Designer.cs
+++ ShopSystem/AdminForm.Designer.cs
@@ -35,7 +35,6 @@
             this.productEntryStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.productUpdateStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.allProductListStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnBack = new System.Windows.Forms.Button();
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.saleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salePunchStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,23 +43,27 @@
             this.inventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.productReceiveStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.StackStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.btnShowMenue = new System.Windows.Forms.Button();
             this.menuStrip4 = new System.Windows.Forms.MenuStrip();
             this.masterSetupToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.addMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.showMemberListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnShowMenue = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linklblUserName = new System.Windows.Forms.LinkLabel();
             this.pnlMenue.SuspendLayout();
             this.panel2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.menuStrip2.SuspendLayout();
             this.menuStrip3.SuspendLayout();
+            this.menuStrip4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
-            this.menuStrip4.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMenue
@@ -77,6 +80,7 @@
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.MidnightBlue;
             this.panel2.Controls.Add(this.menuStrip1);
             this.panel2.Controls.Add(this.menuStrip2);
             this.panel2.Controls.Add(this.menuStrip3);
@@ -107,6 +111,7 @@
             this.productUpdateStripMenuItem,
             this.allProductListStripMenuItem});
             this.masterSetupToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.masterSetupToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.masterSetupToolStripMenuItem.Name = "masterSetupToolStripMenuItem";
             this.masterSetupToolStripMenuItem.Size = new System.Drawing.Size(177, 36);
             this.masterSetupToolStripMenuItem.Text = "Product Setup";
@@ -131,18 +136,6 @@
             this.allProductListStripMenuItem.Size = new System.Drawing.Size(254, 36);
             this.allProductListStripMenuItem.Text = "All Product List";
             // 
-            // btnBack
-            // 
-            this.btnBack.BackColor = System.Drawing.Color.SteelBlue;
-            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnBack.Location = new System.Drawing.Point(474, 275);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(112, 42);
-            this.btnBack.TabIndex = 1;
-            this.btnBack.Text = "Logout";
-            this.btnBack.UseVisualStyleBackColor = false;
-            this.btnBack.Click += new System.EventHandler(this.btnLogout_Click);
-            // 
             // menuStrip2
             // 
             this.menuStrip2.BackColor = System.Drawing.Color.MidnightBlue;
@@ -161,6 +154,7 @@
             this.salePunchStripMenuItem,
             this.saleSummaryStripMenuItem});
             this.saleToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.saleToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.saleToolStripMenuItem.Name = "saleToolStripMenuItem";
             this.saleToolStripMenuItem.Size = new System.Drawing.Size(70, 36);
             this.saleToolStripMenuItem.Text = "Sale";
@@ -196,6 +190,7 @@
             this.productReceiveStripMenuItem,
             this.StackStripMenuItem});
             this.inventoryToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.inventoryToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.inventoryToolStripMenuItem.Name = "inventoryToolStripMenuItem";
             this.inventoryToolStripMenuItem.Size = new System.Drawing.Size(127, 36);
             this.inventoryToolStripMenuItem.Text = "Inventory";
@@ -213,67 +208,6 @@
             this.StackStripMenuItem.Size = new System.Drawing.Size(265, 36);
             this.StackStripMenuItem.Text = "Stock ";
             this.StackStripMenuItem.Click += new System.EventHandler(this.StackStripMenuItem_Click);
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.LightGray;
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.MidnightBlue;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.linkLabel1.LinkColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 40);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(73, 25);
-            this.linkLabel1.TabIndex = 0;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Menue";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel3.Controls.Add(this.btnShowMenue);
-            this.panel3.Controls.Add(this.linkLabel2);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(214, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(616, 112);
-            this.panel3.TabIndex = 2;
-            // 
-            // linkLabel2
-            // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.linkLabel2.Location = new System.Drawing.Point(230, 40);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(156, 25);
-            this.linkLabel2.TabIndex = 0;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "Welcome Admin";
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panel4.Controls.Add(this.btnBack);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel4.Location = new System.Drawing.Point(214, 108);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(616, 345);
-            this.panel4.TabIndex = 3;
-            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
-            // 
-            // btnShowMenue
-            // 
-            this.btnShowMenue.BackColor = System.Drawing.Color.Honeydew;
-            this.btnShowMenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.btnShowMenue.Location = new System.Drawing.Point(3, 3);
-            this.btnShowMenue.Name = "btnShowMenue";
-            this.btnShowMenue.Size = new System.Drawing.Size(46, 38);
-            this.btnShowMenue.TabIndex = 17;
-            this.btnShowMenue.Text = "☰";
-            this.btnShowMenue.UseVisualStyleBackColor = false;
-            this.btnShowMenue.Click += new System.EventHandler(this.btnShowMenue_Click);
             // 
             // menuStrip4
             // 
@@ -295,6 +229,7 @@
             this.addMemberToolStripMenuItem,
             this.showMemberListToolStripMenuItem});
             this.masterSetupToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.masterSetupToolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.masterSetupToolStripMenuItem1.Name = "masterSetupToolStripMenuItem1";
             this.masterSetupToolStripMenuItem1.Size = new System.Drawing.Size(168, 36);
             this.masterSetupToolStripMenuItem1.Text = "Master Setup";
@@ -311,6 +246,103 @@
             this.showMemberListToolStripMenuItem.Name = "showMemberListToolStripMenuItem";
             this.showMemberListToolStripMenuItem.Size = new System.Drawing.Size(286, 36);
             this.showMemberListToolStripMenuItem.Text = "Show Member List";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.LightGray;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 31);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(73, 25);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Menue";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnBack.Location = new System.Drawing.Point(492, 11);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(112, 42);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Logout";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.btnShowMenue);
+            this.panel3.Controls.Add(this.linkLabel2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(214, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(616, 112);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnShowMenue
+            // 
+            this.btnShowMenue.BackColor = System.Drawing.Color.Honeydew;
+            this.btnShowMenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.btnShowMenue.Location = new System.Drawing.Point(3, 3);
+            this.btnShowMenue.Name = "btnShowMenue";
+            this.btnShowMenue.Size = new System.Drawing.Size(46, 38);
+            this.btnShowMenue.TabIndex = 17;
+            this.btnShowMenue.Text = "☰";
+            this.btnShowMenue.UseVisualStyleBackColor = false;
+            this.btnShowMenue.Click += new System.EventHandler(this.btnShowMenue_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.linkLabel2.Location = new System.Drawing.Point(230, 40);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(156, 25);
+            this.linkLabel2.TabIndex = 0;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Welcome Admin";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel4.Controls.Add(this.panel1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(214, 111);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(616, 342);
+            this.panel4.TabIndex = 3;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.linklblUserName);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(616, 65);
+            this.panel1.TabIndex = 2;
+            // 
+            // linklblUserName
+            // 
+            this.linklblUserName.AutoSize = true;
+            this.linklblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.linklblUserName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.linklblUserName.Location = new System.Drawing.Point(38, 20);
+            this.linklblUserName.Name = "linklblUserName";
+            this.linklblUserName.Size = new System.Drawing.Size(134, 25);
+            this.linklblUserName.TabIndex = 2;
+            this.linklblUserName.TabStop = true;
+            this.linklblUserName.Text = "Current User: ";
+            this.linklblUserName.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklblUserName_LinkClicked);
             // 
             // AdminForm
             // 
@@ -334,11 +366,13 @@
             this.menuStrip2.PerformLayout();
             this.menuStrip3.ResumeLayout(false);
             this.menuStrip3.PerformLayout();
+            this.menuStrip4.ResumeLayout(false);
+            this.menuStrip4.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
-            this.menuStrip4.ResumeLayout(false);
-            this.menuStrip4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -370,5 +404,7 @@
         private System.Windows.Forms.ToolStripMenuItem masterSetupToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem addMemberToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem showMemberListToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linklblUserName;
     }
 }
