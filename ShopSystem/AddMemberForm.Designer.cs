5574cae0f9151261f1efb1462b4df824cec1feb3
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-07 17:15:46 +0600

    Update AddMemberForm.Designer.cs

--- ShopSystem/AddMemberForm.Designer.cs
+++ ShopSystem/AddMemberForm.Designer.cs
@@ -37,9 +37,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtRole = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvMemberInfo = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // txtId
@@ -112,7 +120,8 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.btnBack);
             this.panel1.Controls.Add(this.btnAdd);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.txtRole);
@@ -125,27 +134,87 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(362, 450);
+            this.panel1.Size = new System.Drawing.Size(385, 450);
             this.panel1.TabIndex = 8;
             // 
-            // button1
+            // btnBack
             // 
-            this.button1.Location = new System.Drawing.Point(188, 310);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 40);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Add";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBack.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnBack.Location = new System.Drawing.Point(285, 398);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(94, 40);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(174, 225);
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnAdd.Location = new System.Drawing.Point(198, 216);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(94, 40);
+            this.btnAdd.Size = new System.Drawing.Size(70, 42);
             this.btnAdd.TabIndex = 9;
             this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgvMemberInfo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(385, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(415, 450);
+            this.panel2.TabIndex = 9;
+            // 
+            // dgvMemberInfo
+            // 
+            this.dgvMemberInfo.AllowUserToAddRows = false;
+            this.dgvMemberInfo.AllowUserToDeleteRows = false;
+            this.dgvMemberInfo.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dgvMemberInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMemberInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvMemberInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvMemberInfo.Location = new System.Drawing.Point(0, 0);
+            this.dgvMemberInfo.Name = "dgvMemberInfo";
+            this.dgvMemberInfo.ReadOnly = true;
+            this.dgvMemberInfo.Size = new System.Drawing.Size(415, 450);
+            this.dgvMemberInfo.TabIndex = 0;
+            this.dgvMemberInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMemberInfo_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Id";
+            this.Column1.HeaderText = "ID ";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Name";
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Password";
+            this.Column3.HeaderText = "Password";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Role";
+            this.Column4.HeaderText = "Role";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
             // 
             // AddMemberForm
             // 
@@ -153,11 +222,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "AddMemberForm";
             this.Text = "Add Member";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddMemberForm_FormClosed);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMemberInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -174,6 +247,12 @@
         private System.Windows.Forms.TextBox txtRole;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvMemberInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
     }
 }
