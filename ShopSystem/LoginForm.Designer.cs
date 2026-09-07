d7562832035e4b5f8442e7da4e6cccc897aca7c5
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-07 17:16:05 +0600

    Update LoginForm.Designer.cs

--- ShopSystem/LoginForm.Designer.cs
+++ ShopSystem/LoginForm.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.btnLogin = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtId = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,12 +50,33 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.txtPassword);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.txtId);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.checkBox1.Location = new System.Drawing.Point(262, 234);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(94, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Remember me";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(277, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Please enter your information";
             // 
             // linkLabel1
             // 
@@ -105,38 +126,17 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label1.Location = new System.Drawing.Point(164, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(61, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Username";
+            this.label1.Text = "User Id";
             // 
-            // txtUsername
+            // txtId
             // 
-            this.txtUsername.Location = new System.Drawing.Point(262, 141);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(287, 20);
-            this.txtUsername.TabIndex = 0;
-            this.txtUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(277, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(215, 20);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Please enter your information";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.checkBox1.Location = new System.Drawing.Point(262, 234);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(94, 17);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Remember me";
-            this.checkBox1.UseVisualStyleBackColor = false;
+            this.txtId.Location = new System.Drawing.Point(262, 141);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(287, 20);
+            this.txtId.TabIndex = 0;
+            this.txtId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // LoginForm
             // 
@@ -159,7 +159,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label3;
