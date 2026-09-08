6f2fb8fa03ccf5dcba035de9f5d8ca06aa937950
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-08 23:12:46 +0600

    Create AllProductsListForm.cs

---
+++ ShopSystem/AllProductsListForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopSystem
+{
+    public partial class AllProductsListForm : Form
+    {
+        public AllProductsListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackfromPE_Click(object sender, EventArgs e)
+        {
+            AdminForm ad=new AdminForm();
+            ad.Show();
+            this.Visible = false;
+        }
+    }
+}
