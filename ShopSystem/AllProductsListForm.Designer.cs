2b570d24d5ffd143e6d816ef38dc16f874386ed4
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-08 23:12:48 +0600

    Create AllProductsListForm.Designer.cs

---
+++ ShopSystem/AllProductsListForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ShopSystem
+{
+    partial class AllProductsListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBackfromPE = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBackfromPE
+            // 
+            this.btnBackfromPE.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBackfromPE.Location = new System.Drawing.Point(624, 384);
+            this.btnBackfromPE.Name = "btnBackfromPE";
+            this.btnBackfromPE.Size = new System.Drawing.Size(155, 54);
+            this.btnBackfromPE.TabIndex = 1;
+            this.btnBackfromPE.Text = "Back";
+            this.btnBackfromPE.UseVisualStyleBackColor = false;
+            this.btnBackfromPE.Click += new System.EventHandler(this.btnBackfromPE_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 82);
+            this.panel1.TabIndex = 2;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.linkLabel1.Location = new System.Drawing.Point(256, 22);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(234, 31);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Stock Product List";
+            // 
+            // AllProductsListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBackfromPE);
+            this.Name = "AllProductsListForm";
+            this.Text = "All Products List";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackfromPE;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
