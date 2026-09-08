d846723a713ec252867bbbcdb01ecd502df76495
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-09 00:32:41 +0600

    Update AllProductsListForm.Designer.cs

--- ShopSystem/AllProductsListForm.Designer.cs
+++ ShopSystem/AllProductsListForm.Designer.cs
@@ -31,15 +31,28 @@
             this.btnBackfromPE = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvProductInfo = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBackfromPE
             // 
-            this.btnBackfromPE.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnBackfromPE.Location = new System.Drawing.Point(624, 384);
+            this.btnBackfromPE.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnBackfromPE.Location = new System.Drawing.Point(143, 299);
             this.btnBackfromPE.Name = "btnBackfromPE";
-            this.btnBackfromPE.Size = new System.Drawing.Size(155, 54);
+            this.btnBackfromPE.Size = new System.Drawing.Size(102, 46);
             this.btnBackfromPE.TabIndex = 1;
             this.btnBackfromPE.Text = "Back";
             this.btnBackfromPE.UseVisualStyleBackColor = false;
@@ -52,7 +65,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 82);
+            this.panel1.Size = new System.Drawing.Size(703, 82);
             this.panel1.TabIndex = 2;
             // 
             // linkLabel1
@@ -61,24 +74,121 @@
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.linkLabel1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.linkLabel1.Location = new System.Drawing.Point(256, 22);
+            this.linkLabel1.Location = new System.Drawing.Point(232, 20);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(234, 31);
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Stock Product List";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.Controls.Add(this.btnSearch);
+            this.panel2.Controls.Add(this.txtSearch);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnBackfromPE);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 82);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(257, 357);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Navy;
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSearch.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnSearch.Location = new System.Drawing.Point(96, 120);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(102, 46);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtSearch.Location = new System.Drawing.Point(34, 83);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(164, 26);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.Text = "Search By Code";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Location = new System.Drawing.Point(38, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Product Search";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgvProductInfo);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(251, 82);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(452, 357);
+            this.panel3.TabIndex = 4;
+            // 
+            // dgvProductInfo
+            // 
+            this.dgvProductInfo.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dgvProductInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvProductInfo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dgvProductInfo.Location = new System.Drawing.Point(3, 0);
+            this.dgvProductInfo.Name = "dgvProductInfo";
+            this.dgvProductInfo.Size = new System.Drawing.Size(449, 357);
+            this.dgvProductInfo.TabIndex = 0;
+            this.dgvProductInfo.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProductInfo_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Code";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Price";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Quantity";
+            this.Column4.Name = "Column4";
+            // 
             // AllProductsListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(703, 439);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnBackfromPE);
             this.Name = "AllProductsListForm";
             this.Text = "All Products List";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AllProductsListForm_FormClosed);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -88,5 +198,15 @@
         private System.Windows.Forms.Button btnBackfromPE;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvProductInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
     }
 }
