e92f2bb913c08279a1f08997790e2cb2d419318e
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-09 01:30:01 +0600

    Update AllProductsListForm.cs

--- ShopSystem/AllProductsListForm.cs
+++ ShopSystem/AllProductsListForm.cs
@@ -46,5 +46,31 @@ namespace ShopSystem
         {
             Application.Exit();
         }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string code= this.txtSearch.Text;
+                this.txtSearch.Clear();
+                if (string.IsNullOrEmpty(code))
+                {
+                    MessageBox.Show("Please Enter a Product Code");
+                }
+                string sql = "Select * from ProductInfoTable where code= '"+code+"';";
+                DataSet ds= this.FDS.ExecuteQuery(sql);
+                this.dgvProductInfo.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Error.\n"+ex.Message);
+            }
+        }
     }
 }
