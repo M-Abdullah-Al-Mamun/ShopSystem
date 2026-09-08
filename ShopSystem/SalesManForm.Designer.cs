dc45d9e7088e414b37887757b062098dc4275901
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-09 04:57:57 +0600

    Create SalesManForm.Designer.cs

---
+++ ShopSystem/SalesManForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ShopSystem
+{
+    partial class SalesManForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSaleProduct = new System.Windows.Forms.Button();
+            this.btnProductList = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 109);
+            this.panel1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(246, 32);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(266, 31);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Sales Man Dashbord";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.btnProductList);
+            this.panel2.Controls.Add(this.btnSaleProduct);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 109);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(191, 341);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnSaleProduct
+            // 
+            this.btnSaleProduct.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSaleProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSaleProduct.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSaleProduct.Location = new System.Drawing.Point(28, 34);
+            this.btnSaleProduct.Name = "btnSaleProduct";
+            this.btnSaleProduct.Size = new System.Drawing.Size(117, 56);
+            this.btnSaleProduct.TabIndex = 0;
+            this.btnSaleProduct.Text = "Sale Product";
+            this.btnSaleProduct.UseVisualStyleBackColor = false;
+            this.btnSaleProduct.Click += new System.EventHandler(this.btnSaleProduct_Click);
+            // 
+            // btnProductList
+            // 
+            this.btnProductList.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnProductList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnProductList.Location = new System.Drawing.Point(28, 96);
+            this.btnProductList.Name = "btnProductList";
+            this.btnProductList.Size = new System.Drawing.Size(117, 56);
+            this.btnProductList.TabIndex = 1;
+            this.btnProductList.Text = "Product List";
+            this.btnProductList.UseVisualStyleBackColor = false;
+            this.btnProductList.Click += new System.EventHandler(this.btnProductList_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel3.Controls.Add(this.btnLogout);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(191, 374);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(609, 76);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogout.Location = new System.Drawing.Point(492, 17);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(105, 47);
+            this.btnLogout.TabIndex = 1;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // SalesManForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "SalesManForm";
+            this.Text = "Sales Man Form";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SalesManForm_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnProductList;
+        private System.Windows.Forms.Button btnSaleProduct;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
