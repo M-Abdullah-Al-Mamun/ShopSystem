6e124a7d7eeed664a8e6a5b9c5d70f9b2f058dd7
ttonai22 <tahiatonay2002@gmail.com>
2026-09-08 21:27:42 +0600

    Create SalesmanForm.cs

---
+++ ShopSystem/SalesmanForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopSystem
+{
+    public partial class SalesmanForm : Form
+    {
+        public SalesmanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_saleentry_Click(object sender, EventArgs e)
+        {
+            SaleEntryForm sef = new SaleEntryForm();
+            sef.Show();
+        }
+
+        private void button_salesummary_Click(object sender, EventArgs e)
+        {
+            SaleSummaryForm ssf = new SaleSummaryForm();
+            ssf.Show();
+        }
+    }
+}
