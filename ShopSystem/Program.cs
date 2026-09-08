af114a7dca36ef9030a2ffeac21c3b72942aa4b6
MD-Abdullah-Al-Mamun <m.abdullah54525@gmail.com>
2026-09-09 04:58:13 +0600

    Update Program.cs

--- ShopSystem/Program.cs
+++ ShopSystem/Program.cs
@@ -16,7 +16,7 @@ namespace ShopSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManagerForm());
+            Application.Run(new LoginForm());
         }
     }
 }
